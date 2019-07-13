d4586dbcb02bbf7c894d0b5c13c7a60ad215c7a1
Shareef <macbookair@macbooks-macbook-air.home>
2019-07-13 15:37:50 +0300

    practice

--- lists.py
+++ lists.py
@@ -1,2 +1,39 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
 
+# Creating lists 
+number = [1, 2, 4, 5, 7]
+fruits = ['apple', 'orange', 'mango', 'banana']
+
+# print(number)
+
+# Get the value
+print(fruits[3])
+
+# Get the length 
+print(len(fruits))
+
+# Append to the end of th elist 
+fruits.append('grapes')
+
+#  Remove from the list 
+fruits.remove('mango')
+
+# Change the value of an element
+fruits[2] = 'jelly'
+
+# Insert to a specific position
+fruits.insert(3, 'strawbwrries')
+
+# Remove from specific position 
+fruits.pop(4)
+
+# Reverse the list 
+fruits.reverse()
+
+# Sort list
+fruits.sort()
+
+# Reverse sort list (will sort the list in the oposite direction)
+fruits.sort(reverse = True)
+
+print(fruits)
--- tuples_sets.py
+++ tuples_sets.py
@@ -1,5 +1,36 @@
-# A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
- 
+# A Tuple is a collection which is ordered and unchangeable. Allows duplicate members. 
+# Creating tuples 
+fruits = ('apple', 'oranges', 'banana')
+fruits2 = ('apple')
 
+# Get value
+# print(fruits[0])
+
+# Delete tuple 
+# del fruits2
+# print(fruits2)
+
+# Get length of tuple 
+print(len(fruits))
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+
+vegitables = {'carrot', 'beans', 'tomato'}
+
+# to check if elm is in set 
+print('beans' in vegitables)
+
+# To add to set 
+vegitables.add('qugumber')
+# print(vegitables)
+
+# Remove from set 
+vegitables.remove('beans')
+
+# Clear set 
+vegitables.clear()
+
+# Delete set
+# del vegitables
+
+print(vegitables)
